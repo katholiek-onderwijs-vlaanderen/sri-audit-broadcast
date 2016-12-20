2e9e3af924ddc94da8e0f03cde111d0e874e2e4d
guntherclaes <guntherclaes@lemonade.be>
2016-12-20 16:16:41 +0100

    remove extra field before test

--- js/versions.js
+++ js/versions.js
@@ -64,6 +64,8 @@ module.exports = {
     query.sql('SELECT * FROM versions WHERE key = ').param(elm.key);
     $u.executeSQL(database, query)
       .then(function(data){
+        removePersonContactDetailsFromJSON(calcType(elm), elm);
+        removeDollarDollarFieldsFromJSON(elm);
         if (data.rows.length > 0 && !_.isEqual(data.rows[0].document, elm.document)) {
               deferred.reject({
                 statusCode: 409,
